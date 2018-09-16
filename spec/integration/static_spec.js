b9964c9436dd4db28aee7d0c09cbcd3e8519d64f
spjohnson <spgant@gmail.com>
2018-09-16 08:36:52 -0600

    figuring out server error 500

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,4 +12,13 @@ describe('routes: static', () => {
             });
         });
     });
+    describe('GET /about', () => {
+        it('should return status code 200 and have About us string in the body of the response', (done) => {
+            request.get(base, (err, result, body) => {
+                expect(result.statusCode).toBe(200); 
+                expect(body).toContain('About Us'); 
+                done(); 
+            }); 
+        }); 
+    }); 
 });
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -3,3 +3,9 @@ module.exports = {
         res.render('static/index', { title: 'Welcome to Bloccit' });
     }
 }
+
+// module.exports = {
+//     index(req, res, next){
+//         res.render('static.index', { about: 'About Us'}); 
+//     }
+// }
