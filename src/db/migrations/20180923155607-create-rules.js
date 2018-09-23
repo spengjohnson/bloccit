c5f37dee69bd0326231e24d291e02a39a44a6ec4
spjohnson <spgant@gmail.com>
2018-09-23 11:34:24 -0600

    fixed error with curly braces in rule migrations file

--- src/db/migrations/20180923155607-create-rules.js
+++ src/db/migrations/20180923155607-create-rules.js
@@ -1,38 +1,39 @@
 'use strict';
 module.exports = {
-        up: (queryInterface, Sequelize) => {
-            return queryInterface.createTable('Rules', {
-                    id: {
-                        allowNull: false,
-                        autoIncrement: true,
-                        primaryKey: true,
-                        type: Sequelize.INTEGER
-                    },
-                    description: {
-                        type: Sequelize.STRING
-                    },
-                    createdAt: {
-                        allowNull: false,
-                        type: Sequelize.DATE
-                    },
-                    updatedAt: {
-                        allowNull: false,
-                        type: Sequelize.DATE
-                    },
-                    topicId: {
-                        type: Sequelize.INTEGER,
-                        onDelete: "CASCADE",
-                        references: {
-                            model: "Topic",
-                            key: "id",
-                            as: "topicId",
-                        }
-                    },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Rules', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            description: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            topicId: {
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                references: {
+                    model: "Topic",
+                    key: "id",
+                    as: "topicId",
                 }
+            }
+        });
 
-            });
+
     },
     down: (queryInterface, Sequelize) => {
         return queryInterface.dropTable('Rules');
     }
-};
+
+}
